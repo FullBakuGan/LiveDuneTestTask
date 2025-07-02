381f2015324c0d0b54243bcfb18ab364f515715c
FullBakuGan <nikosmelik666@gmail.com>
2025-07-03 02:40:14 +0300

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    strictPort: true,
-  },
-  plugins: [react()],
-  base: '/LiveDuneTestTask/',
+  base: '/LiveDuneTestTask/', 
+  build: {
+    outDir: 'docs' 
+  }
 })
