897bcaddc2c040eed17cb7ae1bc8db363a0457a4
FullBakuGan <nikosmelik666@gmail.com>
2025-07-03 04:26:02 +0300

    вапвпа

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/LiveDuneTestTask/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'docs',
-    emptyOutDir: true, 
+    emptyOutDir: true,
+    assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg'],
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
   },
   server: {
-    historyApiFallback: true 
-}
+    historyApiFallback: true,
+  }
 })
