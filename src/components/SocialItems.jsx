f63d8ba11fc0666ea851df425e8044bee4807a3a
FullBakuGan <nikosmelik666@gmail.com>
2025-07-03 05:32:37 +0300

    рвапврв

--- src/components/SocialItems.jsx
+++ src/components/SocialItems.jsx
@@ -1,17 +1,22 @@
 import './Social-items.css';
 import React from 'react';
-import { SocialItemsImages } from '../data';
 
 export default function SocialItems() {
   return (
     <div className="social-auth">
       <div className="social-icons">
         <div className="social-item facebook">
-          <img src={SocialItemsImages.facebook} alt="Facebook" />
+          <img 
+            src={`${process.env.PUBLIC_URL}/facebook-logo.svg`} 
+            alt="Facebook" 
+          />
           <span>Войти через Facebook</span>
         </div>
         <div className="social-item google">
-          <img src={SocialItemsImages.google} alt="Google" />
+          <img 
+            src={`${process.env.PUBLIC_URL}/google-logo.png`} 
+            alt="Google" 
+          />
           <span>Войти через Google</span>
         </div>
       </div>
