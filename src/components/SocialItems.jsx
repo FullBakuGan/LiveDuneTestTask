54cb2a8b6aa239f11f9708952212e683a46ee06e
FullBakuGan <nikosmelik666@gmail.com>
2025-07-03 06:13:07 +0300

    Revert "Fix image paths"
    This reverts commit e0023d467f3c8195c0aed57d7f29526126c812fa.

--- src/components/SocialItems.jsx
+++ src/components/SocialItems.jsx
@@ -2,21 +2,19 @@ import './Social-items.css';
 import React from 'react';
 
 export default function SocialItems() {
-  const baseUrl = '/LiveDuneTestTask';
-
   return (
     <div className="social-auth">
       <div className="social-icons">
         <div className="social-item facebook">
           <img 
-            src={`${baseUrl}/facebook-logo.svg`} 
+            src={`${process.env.PUBLIC_URL}/facebook-logo.svg`} 
             alt="Facebook" 
           />
           <span>Войти через Facebook</span>
         </div>
         <div className="social-item google">
           <img 
-            src={`${baseUrl}/google-logo.png`} 
+            src={`${process.env.PUBLIC_URL}/google-logo.png`} 
             alt="Google" 
           />
           <span>Войти через Google</span>
